035c2006e3023a00c02976a74fdaacc212f7be6f
Hack Reactor Student <student@hackreactor.com>
2017-07-28 19:53:36 -0700

    Draft schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,30 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) DEFAULT 'anonymous',
+  message TEXT,
+  roomname VARCHAR(40) DEFAULT 'lobby',
+  createdAt DATE NOT NULL
+  
+
+
+
+);
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(40) DEFAULT 'lobby'
+
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(30) DEFAULT 'anonymous'
 );
 
 /* Create other tables and define schemas for them here! */
